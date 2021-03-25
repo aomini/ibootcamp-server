494d79f577e66cd833e686afc34fb22953dc9627
rakeshshubhu <rak3sh.shrestha@gmail.com>
2021-03-25 20:50:12 +0545

    compilted with ts

---
+++ src/App.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+router.get("/test", function (_, res) {
+    res.send("this is a test");
+});
+app.use("/", router);
+var port = 5000;
+app.listen(port, function () {
+    console.log("your server is running");
+});
